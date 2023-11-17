1da717ffbbf8376549cc06ff0ab1bb177e75cefe
nikodego <nikkobryllealturas716@gmail.com>
2023-11-17 18:50:23 +1100

    1st commit

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Data } from './Datas';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Data/>
     </View>
   );
 }
@@ -15,6 +15,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+    marginTop: 20,
   },
 });
